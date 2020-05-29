440d7c21fe8018a58a5c31186cd167f8fe6f9d9c
Yusuke57 <honey57g@gmail.com>
2020-05-29 09:08:20 +0900

    修正

--- js/common.js
+++ js/common.js
@@ -1,11 +1,11 @@
-$(function(){
+$(function () {
 
   // 共通パーツ
-   $("header").load("/portfolio/layout/header.html");
-   $("footer").load("/portfolio/layout/footer.html");
+  $("header").load("../layout/header.html");
+  $("footer").load("../layout/footer.html");
 
   // サムネ取得
-  $(".cover-image").each(function(i, elem){
+  $(".cover-image").each(function (i, elem) {
     var $path = $(elem).children("a.content").attr("href");
     $(elem).css("background-image", "url('" + $path + "img/thumb.png')");
   });
