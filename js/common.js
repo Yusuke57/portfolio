ea7b3578a4bd5a73dde89fd2a4455e53c30495bb
Yusuke57 <honey57g@gmail.com>
2020-05-30 09:22:24 +0900

    カテゴリ分け

--- js/common.js
+++ js/common.js
@@ -10,4 +10,58 @@ $(function () {
     $(elem).css("background-image", "url('" + $path + "img/thumb.png')");
   });
 
+
+  // 作品クラス
+  function Work(permalink, title, category, subCategory, date) {
+    this.permalink = permalink;
+    this.title = title;
+    this.category = category;
+    this.subCategory = subCategory;
+    this.date = date;
+  }
+
+  // カテゴリ
+  var Category = {
+    Game: "game",
+    Web: "web",
+    Other: "other",
+  };
+
+  // 作品一覧
+  const works = [
+    new Work("star-trips", "STAR TRIPS", Category.Game, "OpenGLブラウザゲーム", "2016-2-12"),
+  ];
+
+
+
+
+
+  // 作品情報を挿入
+  function getWorkHtml(workData) {
+    var html = '<div class="cover-image" style="background-image: ' + 'url("' + workData.category + '/' + workData.permalink + 'img/thumb.png")' + '>\n'
+    html += '<a class="content" href="';
+    html += workData.category + '/';
+    html += workData.permalink + '/">\n';
+    html += '<div class="overlay-text">\n';
+    html += '<div class="content-title">' + workData.title + '</div>\n';
+    html += '<div class="content-category">' + workData.subCategory + '</div>\n';
+    html += '<div class="content-title">' + workData.date + '</div>\n';
+    html += '</div>\n</a>\n</div>\n\n';
+
+    return html;
+  }
+
+  function getWorksHtmlByCategory(category) {
+    var html = "";
+    works.forEach(data => {
+      if (data.category == category) {
+        html += getWorkHtml(data);
+      }
+    });
+    return html;
+  }
+
+  $('.work-list-game').prepend(getWorksHtmlByCategory(Category.Game));
+
+
 });
